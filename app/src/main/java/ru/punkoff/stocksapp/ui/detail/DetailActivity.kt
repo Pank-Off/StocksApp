9b587461c9c8db0743cb2be3d560b401d4c6f728
Punk_Off <pankov.kirill1997@yandex.ru>
2021-03-22 23:40:20 +0300

    show graph correct

--- app/src/main/java/ru/punkoff/stocksapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/ru/punkoff/stocksapp/ui/detail/DetailActivity.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.jjoe64.graphview.DefaultLabelFormatter
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.punkoff.stocksapp.R
 import ru.punkoff.stocksapp.databinding.ActivityDetailBinding
 import ru.punkoff.stocksapp.model.Stock
 import ru.punkoff.stocksapp.model.retrofit.Candle
@@ -52,12 +55,18 @@ class DetailActivity : AppCompatActivity() {
                         "\uD83D\uDE28 Wooops $result.message}",
                         Toast.LENGTH_LONG
                     ).show()
-                    binding.loadingBar.visibility = View.INVISIBLE
-                    binding.retryBtn.visibility = View.VISIBLE
+                    with(binding) {
+                        loadingBar.visibility = View.INVISIBLE
+                        retryBtn.visibility = View.VISIBLE
+                    }
+
                 }
                 StocksViewState.Loading -> {
-                    binding.loadingBar.visibility = View.VISIBLE
-                    binding.retryBtn.visibility = View.INVISIBLE
+                    with(binding) {
+                        binding.loadingBar.visibility = View.VISIBLE
+                        binding.retryBtn.visibility = View.INVISIBLE
+                    }
+
                 }
                 is StocksViewState.StockValue -> Unit
             }
@@ -73,24 +82,42 @@ class DetailActivity : AppCompatActivity() {
 
     private fun plotGraph(candle: Candle) {
         val listOfCandles = mutableListOf<DataPoint>()
-        val simpleDateFormat = SimpleDateFormat("H.mm", Locale.ENGLISH)
+        val simpleDateFormat = SimpleDateFormat("H:mm", Locale.ENGLISH)
         simpleDateFormat.timeZone = TimeZone.getTimeZone("Europe/Moscow")
         for ((index, element) in candle.openPrice.withIndex()) {
             val time = Date(candle.time[index] * 1000)
-            val dateString = simpleDateFormat.format(time)
-            listOfCandles.add(DataPoint(dateString.toDouble(), element))
+            listOfCandles.add(DataPoint(time, element))
         }
         val series = LineGraphSeries(
             listOfCandles.toTypedArray()
         )
-        binding.graph.addSeries(series)
+        val dayFormat = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
+        val graphTitleBuilder = StringBuilder("Stock candles for ")
+        graphTitleBuilder.append(dayFormat.format(listOfCandles[0].x))
+        with(binding) {
+            graph.title = graphTitleBuilder.toString()
+            graph.addSeries(series)
+            graph.gridLabelRenderer.numHorizontalLabels = 5
+            graph.gridLabelRenderer.setHumanRounding(false, true)
+            graph.viewport.isXAxisBoundsManual = true
+            graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+                override fun formatLabel(value: Double, isValueX: Boolean): String {
+                    return if (isValueX) {
+                        simpleDateFormat.format(Date(value.toLong()))
+                    } else {
+                        super.formatLabel(value, isValueX)
+                    }
+                }
+            }
+        }
     }
 
     private fun setInitialData(stock: Stock) {
         detailViewModel.getCandles(stock.ticker)
         val currentPriceBuilder = StringBuilder("$")
         currentPriceBuilder.append(stock.price)
-        val changeStockBuilder = StringBuilder(stock.difPrice.toString())
+        val changeStockBuilder = StringBuilder("$")
+        changeStockBuilder.append(stock.difPrice)
         changeStockBuilder.append(" (${stock.stock}%)")
         val btnTextBuilder = java.lang.StringBuilder("Buy for ")
         btnTextBuilder.append(currentPriceBuilder)
@@ -100,6 +127,9 @@ class DetailActivity : AppCompatActivity() {
             price.text = currentPriceBuilder
             changeStock.text = changeStockBuilder
             buyBtn.text = btnTextBuilder
+
+            graph.titleTextSize = resources.getDimension(R.dimen.graphTitle)
+            graph.titleColor = ContextCompat.getColor(this@DetailActivity, R.color.black)
         }
     }
 }
